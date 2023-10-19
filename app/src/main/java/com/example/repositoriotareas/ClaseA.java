b8fa527dd348cceba0536f73634fc35ac856dddf
josego16 <jgomlin@g.educaand,es>
2023-10-19 20:51:20 +0200

    creacion de la claseA y el objeto persona en la main activity

--- app/src/main/java/com/example/repositoriotareas/ClaseA.java
+++ app/src/main/java/com/example/repositoriotareas/ClaseA.java
@@ -1,4 +1,45 @@
 package com.example.repositoriotareas;
 
+import androidx.annotation.NonNull;
+
 public class ClaseA {
+    private String nombre;
+    private String apellidos;
+    private int edad;
+
+    public ClaseA(String nombre, String apellidos, int edad) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return String.format("Nombre %s, Apellido %s, Edad %d", nombre, apellidos, edad);
+    }
 }
--- app/src/main/java/com/example/repositoriotareas/MainActivity.kt
+++ app/src/main/java/com/example/repositoriotareas/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.repositoriotareas
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val persona = ClaseA("josema", "gomez", 22)
+        persona.nombre = "jaime"
+        persona.apellidos = "gomez"
+        persona.edad = 19
+        Toast.makeText(this, persona.toString(), Toast.LENGTH_SHORT).show()
     }
 }
